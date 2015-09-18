924c22a3c1b616aa870725f7f6fc530ec2a302db
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2015-09-18 14:32:30 +0200

    Use own function for walking value nodes

--- src/index.js
+++ src/index.js
@@ -4,29 +4,47 @@ import postcss from 'postcss';
 import convert from './lib/convert';
 import valueParser, {unit} from 'postcss-value-parser';
 
+function processValueNode (opts, node) {
+    if (node.type === 'word') {
+        let pair = unit(node.value);
+        if (pair) {
+            let num = Number(pair.number);
+            let u = pair.unit.toLowerCase();
+            if (num === 0) {
+                node.value = (u === 'ms' || u === 's') ? 0 + u : 0;
+            } else {
+                node.value = convert(num, u, opts);
+            }
+        }
+    }
+    if (node.type === 'function' && node.value !== 'calc') {
+        return false;
+    }
+}
+
+function walk (node, cb) {
+    var i, max, n;
+    if (node.nodes) {
+        for (i = 0, max = node.nodes.length; i < max; i += 1) {
+            n = node.nodes[i];
+            if (false !== cb(node.nodes[i])) {
+                walk(n, cb);
+            }
+        }
+    }
+    return node;
+}
+
 function transform (opts) {
     return decl => {
         if (~decl.prop.indexOf('flex')) {
             return;
         }
 
-        decl.value = valueParser(decl.value).walk(function (node) {
-            if (node.type === 'word') {
-                let pair = unit(node.value);
-                if (pair) {
-                    let num = Number(pair.number);
-                    let u = pair.unit.toLowerCase();
-                    if (num === 0) {
-                        node.value = (u === 'ms' || u === 's') ? 0 + u : 0;
-                    } else {
-                        node.value = convert(num, u, opts);
-                    }
-                }
-            }
-            if (node.type === 'function' && node.value !== 'calc') {
-                return false;
-            }
-        }).toString();
+        decl.value = walk(
+            valueParser(decl.value),
+            processValueNode.bind(null, opts)
+        ).toString();
     };
 }
 
